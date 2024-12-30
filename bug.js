916abd89a556d9033e67d6077a33633ea97f3992
BugHunter-X <askzenai@gmail.com>
2024-12-30 09:30:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+function foo(a, b) {
+  return a + b;
+}
+
+console.log(foo(5, "5")); // Output: "55"
+
+function bar(a, b) {
+  return parseInt(a) + parseInt(b);
+}
+
+console.log(bar(5, "5")); // Output: 10
